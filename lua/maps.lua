a3ee9684682475f8ed0b521313707bc9621006ce
gmrdn <guillaume.rondon@gmail.com>
2024-02-12 21:21:18 +0100

    feat(sf): returned to telescope for sf

--- lua/maps.lua
+++ lua/maps.lua
@@ -46,7 +46,7 @@ keymap.set('n', '<leader>d', ':bdelete<cr>', { silent = true })
 
 -- Mini Picks
 keymap.set('n', '<leader>sd', ':Pick diagnostic<Return>', { silent = true })
-keymap.set('n', '<leader>sf', ':Pick git_files<Return>', { silent = true })
+-- keymap.set('n', '<leader>sf', ':Pick git_files<Return>', { silent = true })
 -- keymap.set('n', '<leader>sg', ':Pick grep_live<Return>', { silent = true })
 keymap.set('n', '<leader>sc', ':Pick git_commits<Return>', { silent = true })
 keymap.set('n', '<leader>sv', ':Pick visit_paths<Return>', { silent = true })
