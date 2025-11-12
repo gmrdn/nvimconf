80fb4438313ebbdde7b4af40684e1af98a61ccfc
Guillaume Rondon <guillaume.rondon@360learning.com>
2025-11-12 10:16:16 +0100

    upgraded treesitter conf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,30 @@
-local telescope = require("telescope")
-local actions = require("telescope.actions")
-local builtin = require('telescope.builtin')
+-- local telescope = require("telescope")
+-- local actions = require("telescope.actions")
+-- local builtin = require('telescope.builtin')
 
 return {
     "nvim-telescope/telescope.nvim",
     dependencies = { 'nvim-lua/plenary.nvim' },
-    keys = {
-        { '<leader>ca', vim.lsp.buf.code_action,   desc = '[C]ode [A]ctions' },
-        { '<leader>cd', vim.diagnostic.open_float, desc = '[C]ode [D]iagnostics' },
-        { '<leader>cr', vim.lsp.buf.rename,        desc = '[C]ode [R]ename' },
-        { 'K',          vim.lsp.buf.hover,         desc = 'Show [K]ind' },
-        {
-            '<leader>f',
-            function()
-                -- You can pass additional configuration to telescope to change theme, layout, etc.
-                builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-                    winblend = 10,
-                    previewer = false,
-                })
-            end,
-            desc = '[/] Fuzzily search in current buffer'
-        },
-        { '<leader>?',  builtin.oldfiles,    desc = '[?] Find recently opened files' },
-        { '<leader>gs', builtin.git_status,  desc = '[G]it [S]tatus' },
-        { '<leader>sh', builtin.help_tags,   desc = '[S]earch [H]elp' },
-        { '<leader>sw', builtin.grep_string, desc = '[S]earch current [W]ord' },
-        { '<leader>sd', builtin.diagnostics, desc = '[S]earch [D]iagnostics' }
-    }
+    -- keys = {
+    --     { '<leader>ca', vim.lsp.buf.code_action,   desc = '[C]ode [A]ctions' },
+    --     { '<leader>cd', vim.diagnostic.open_float, desc = '[C]ode [D]iagnostics' },
+    --     { '<leader>cr', vim.lsp.buf.rename,        desc = '[C]ode [R]ename' },
+    --     { 'K',          vim.lsp.buf.hover,         desc = 'Show [K]ind' },
+    --     {
+    --         '<leader>f',
+    --         function()
+    --             -- You can pass additional configuration to telescope to change theme, layout, etc.
+    --             builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    --                 winblend = 10,
+    --                 previewer = false,
+    --             })
+    --         end,
+    --         desc = '[/] Fuzzily search in current buffer'
+    --     },
+    --     { '<leader>?',  builtin.oldfiles,    desc = '[?] Find recently opened files' },
+    --     { '<leader>gs', builtin.git_status,  desc = '[G]it [S]tatus' },
+    --     { '<leader>sh', builtin.help_tags,   desc = '[S]earch [H]elp' },
+    --     { '<leader>sw', builtin.grep_string, desc = '[S]earch current [W]ord' },
+    --     { '<leader>sd', builtin.diagnostics, desc = '[S]earch [D]iagnostics' }
+    -- }
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,32 +1,31 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    version = false, -- last release is way too old and doesn't work on Windows
+    "nvim-treesitter/nvim-treesitter", 
+    branch = 'master', 
+    lazy = false, 
     build = ":TSUpdate",
-
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            ensure_installed = {
-                "bash",
-                "css",
-                "dockerfile",
-                "go",
-                "html",
-                "javascript",
-                "json",
-                "lua",
-                "markdown",
-                "python",
-                "rust",
-                "toml",
-                "typescript",
-                "vim",
-                "vue",
-                "yaml"
-            },
-        })
-    end,
+    opts = {
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
+        ensure_installed = {
+            "bash",
+            "css",
+            "dockerfile",
+            "go",
+            "html",
+            "javascript",
+            "json",
+            "lua",
+            "markdown",
+            "python",
+            "rust",
+            "toml",
+            "typescript",
+            "vim",
+            "vue",
+            "yaml"
+        },
+    },
 }
+
